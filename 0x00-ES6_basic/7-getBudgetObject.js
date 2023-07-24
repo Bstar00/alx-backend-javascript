7e2e21130c2c5ce6d59f559895782e43c0228e13
Bstar00 <samueloriola212@gmail.com>
2023-07-24 14:17:57 +0000

    using keyname"

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,10 @@
+export default function getBudgetObject(income, gdp, capita) {
+  // Using shorthand property notation
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
