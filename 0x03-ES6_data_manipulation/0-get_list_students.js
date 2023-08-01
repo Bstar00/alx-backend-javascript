6af4989502465a16690e56a4476048c63e7ba603
Bstar00 <samueloriola212@gmail.com>
2023-08-01 07:29:13 +0000

    task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,5 @@
+const getListStudents = () => [
+  { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
+  { firstName: 'James', id: 2, location: 'Columbia' },
+  { firstName: 'Serena', id: 5, location: 'San Francisco' },
+];
